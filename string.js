2cc85336ad00ed1cd46ce713e94a75cc17225357
unknown <shahriarnaiemrafi94@gmail.com>
2026-02-01 23:00:19 +0400

    second

---
+++ string.js
@@ -0,0 +1,16 @@
+const poem ='am pata jora jora\n'+
+'marnu cabula jora jora';
+const po=`rafi is a good
+
+radfsad
+sadfasd
+fdsfas
+
+boy`
+console.log(po);
+function sum(a, b=0) {
+    const total= a + b;
+    const output=`sum of ${a} and ${b} is ${total}`;
+    console.log(output);
+    }
+sum(10);
